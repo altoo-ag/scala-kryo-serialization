cc6723824e748a7840310a06dddeca3195923e8f
Gerard Murphy <gjmurphy1@icloud.com>
2024-09-25 21:24:33 +0100

    Extend `ScalaObjectSerializerTest` with a second test to document the copying behaviour of for `ScalaObjectSerializer`. Test fails, as expected. Now to fix it...

--- core/src/test/scala/io/altoo/serialization/kryo/scala/serializer/ScalaObjectSerializerTest.scala
+++ core/src/test/scala/io/altoo/serialization/kryo/scala/serializer/ScalaObjectSerializerTest.scala
@@ -1,6 +1,5 @@
 package io.altoo.serialization.kryo.scala.serializer
 
-import io.altoo.serialization.kryo.scala.serializer
 import io.altoo.serialization.kryo.scala.testkit.AbstractKryoTest
 
 object standalone
@@ -8,15 +7,27 @@ object standalone
 object ScalaObjectSerializerTest
 
 class ScalaObjectSerializerTest extends AbstractKryoTest {
+  private def configureKryo(): Unit = {
+    kryo.setRegistrationRequired(false)
+    kryo.addDefaultSerializer(classOf[standalone.type], classOf[ScalaObjectSerializer[Any]])
+    kryo.addDefaultSerializer(classOf[ScalaObjectSerializerTest.type], classOf[ScalaObjectSerializer[Any]])
+  }
+
   behavior of "ScalaObjectSerializer"
 
   it should "round trip standalone and companion objects" in {
-    kryo.setRegistrationRequired(false)
-    kryo.addDefaultSerializer(classOf[standalone.type], classOf[ScalaObjectSerializer[Any]])
-    kryo.addDefaultSerializer(classOf[serializer.ScalaObjectSerializerTest.type], classOf[ScalaObjectSerializer[Any]])
+    configureKryo()
 
     testSerializationOf(standalone)
 
     testSerializationOf(ScalaObjectSerializerTest)
+  }
+
+  it should "support copying of standalone and companion objects" in {
+    configureKryo()
+
+    testCopyingOf(standalone)
+
+    testCopyingOf(ScalaObjectSerializerTest)
   }
 }
