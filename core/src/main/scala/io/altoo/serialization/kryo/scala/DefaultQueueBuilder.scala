0b3333cb7cece4b8385b8ca1830a9cf3e659f07a
Daniel Schroeter <dsc@scaling.ch>
2026-03-12 16:51:05 +0100

    remove dependency to agrona upgrading it would require jdk17 and pekko 1 vs 2 would also lead to issues since pekko 2.x uses another major of agrona

--- core/src/main/scala/io/altoo/serialization/kryo/scala/DefaultQueueBuilder.scala
+++ core/src/main/scala/io/altoo/serialization/kryo/scala/DefaultQueueBuilder.scala
@@ -1,19 +1,20 @@
 package io.altoo.serialization.kryo.scala
 
-import org.agrona.concurrent.ManyToManyConcurrentArrayQueue
-
 import java.util
+import java.util.concurrent.ConcurrentLinkedQueue
 import scala.reflect.ClassTag
 
 /**
  * Default queue builder that can be extended to use another type of queue.
  * Notice that it must be a multiple producer and multiple consumer queue type,
  * you could use for example a bounded non-blocking queue.
+ *
+ * Previous versions used org.agrona.concurrent.ManyToManyConcurrentArrayQueue - but hard to measure performance gain over ConcurrentLinkedQueue is marginal and not worth the extra dependency.
  */
 class DefaultQueueBuilder {
 
   /**
    * Override to use a different queue.
    */
-  def build[T: ClassTag]: util.Queue[T] = new ManyToManyConcurrentArrayQueue[T](Runtime.getRuntime.availableProcessors * 4)
+  def build[T: ClassTag]: util.Queue[T] = new ConcurrentLinkedQueue[T]()
 }
